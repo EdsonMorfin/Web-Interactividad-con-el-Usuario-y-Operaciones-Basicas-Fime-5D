68eea8daa3e5e96afb85cb2586c7778382ffc535
Edson <edsonpaul2018@gmail.com>
2021-09-17 13:09:24 -0500

    calculator function working

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function ChangeView(clicked_id){
     }
     else if(clicked_id=="button2"){
         document.getElementById("page2").className ="view hide";
-        document.getElementById("page4").className ="view";
+        document.getElementById("page4").className ="view calculator-grid";
     }
     else if(clicked_id=="relation_operation"){
         document.getElementById("page4").className ="view hide";
@@ -37,6 +37,37 @@ function PrintName(){
     document.getElementById("login_name").innerHTML= name;
     document.getElementById("goodbye_name").innerHTML= name;
 }
+
+function takeValue(value){
+    document.getElementById("inputwindow").value += value;
+}
+
+function clearInput(){
+    document.getElementById("inputwindow").value="";
+}
+
+String.prototype.replaceAt = function(index, replacement) {
+    if (index >= this.length) {
+        return this.valueOf();
+    }
+ 
+    return this.substring(0, index) + replacement + this.substring(index + 1);
+}
+
+function calculateResult(){
+    var value= document.getElementById('inputwindow').value;
+    for(let j=0; j < value.length; j++){
+        if(value.charAt(j)=="x"){
+            value=value.replaceAt(j,'*')
+        }
+
+    }
+
+    var result=eval(value);
+    document.getElementById('inputwindow').value=result;
+}
+
+
 
 
 
