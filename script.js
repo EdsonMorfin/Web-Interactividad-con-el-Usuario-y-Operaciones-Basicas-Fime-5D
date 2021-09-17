8e5f5713e2f4400c89c91b472ef63f4a0cac2dbb
Edson <edsonpaul2018@gmail.com>
2021-09-17 17:04:05 -0500

    calculator rework

--- script.js
+++ script.js
@@ -12,14 +12,17 @@ function ChangeView(clicked_id){
     if (clicked_id=="button1"){
         document.getElementById("page1").className ="view hide";
         document.getElementById("page2").className ="view";
+        document.getElementById("inputwindow").className="view hide"
     }
     else if(clicked_id=="button2"){
         document.getElementById("page2").className ="view hide";
-        document.getElementById("page4").className ="view calculator-grid";
+        document.getElementById("page4").className ="view";
+        document.getElementById("inputwindow").className="view"
     }
     else if(clicked_id=="relation_operation"){
         document.getElementById("page4").className ="view hide";
         document.getElementById("page5").className ="view";
+        document.getElementById("inputwindow").className="view"
     }
     else if(clicked_id=="thanks_button"){
         document.getElementById("page2").className ="view hide";
@@ -62,12 +65,8 @@ function calculateResult(){
         }
 
     }
-
+    
     var result=eval(value);
     document.getElementById('inputwindow').value=result;
 }
-
-
-
-
 
